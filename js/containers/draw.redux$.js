22eea8d29d9625849ac38297d60ea469ada2904a
jansesun <307665751@qq.com>
2017-06-29 19:15:18 +0800

    Fixed draw result only one item bug
    Change-Id: I86f3f2c2501daa09fb1bca03317d361e31921d58

--- js/containers/draw.redux$.js
+++ js/containers/draw.redux$.js
@@ -22,10 +22,10 @@ export const playerReducer$ = Rx.Observable.of(() => playerInitialState)
     })),
     playerActions.reset$.map(() => state => playerInitialState)
   );
-const initialDrawResult = JSON.parse(localStorage.getItem(`drawResult_${TIMESTAMP}`)) || [];
+const initialDrawResult = JSON.parse(localStorage.getItem(`drawResult_${TIMESTAMP}`)) || [[]];
 const initialState = {
   playerList: JSON.parse(localStorage.getItem(`playerList_${TIMESTAMP}`)) || [],
-  drawResult: initialDrawResult.length > 1 ? initialDrawResult : []
+  drawResult: initialDrawResult[0].length === 2 ? initialDrawResult : []
 };
 export const indexReducer$ = Rx.Observable.of(() => initialState)
   .merge(
