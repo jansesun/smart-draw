08c94d54cf692619dc538e7d6e4017d7e5349352
jansesun <307665751@qq.com>
2017-06-29 17:11:48 +0800

    Supported release to blog
    Change-Id: Ie1f805729800f1847bb0cdb52496746f982029c3

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,40 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var isDebug = process.env.NODE_ENV === 'development';
+var hash = isDebug ? '' : '.[chunkhash:8]';
+var plugins = [];
+if(isDebug) {
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './jade/index.jade',
+      inject: true,
+      chunks: ['draw']
+    })
+  );
+} else {
+  plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      minimize: true
+    })
+  );
+}
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: isDebug ? 'eval-source-map' : false,
   output: {
     path: path.join(__dirname, 'release'),
-    filename: 'js/[name].js',
-    publicPath: '//localhost:3000/static/'
+    filename: 'js/[name]' + hash + '.js',
+    publicPath: isDebug ? '//localhost:3000/static/' : '//jansesun.github.io/'
   },
   entry: {
     'draw': ['./js/page/draw']
@@ -25,15 +53,7 @@ module.exports = {
       }
     ],
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './jade/index.jade',
-      inject: true,
-      chunks: ['draw']
-    })
-  ],
+  plugins: plugins,
   resolve: {
     extensions: ['.js', '.jsx']
   },
